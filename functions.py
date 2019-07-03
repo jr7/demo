c054d358eee6afbbcec1080d73646283df8bb7d6
Jonas Ruebsam <jonas.ruebsam@avl.com>
2019-07-03 13:56:12 +0200

    Refactor plotting functions into module

---
+++ functions.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def sin(x):
+    return np.sin(x)
+
+def cos(x):
+    return np.cos(x)
--- plot.py
+++ plot.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from functions import sin, cos
+
 x  = np.linspace(0, 10, 10000)
-plt.plot(x, np.sin(x))
-plt.plot(x, np.cos(x))
+plt.plot(x, sin(x))
+plt.plot(x, cos(x))
 
 plt.show()
